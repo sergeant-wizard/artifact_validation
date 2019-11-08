00ff8976ccfa9a04f7e1c57b53320255bf9150c8
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-11-07 16:36:19 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='artifact_validation',
+    version='0.0.0',
+    description='Keep real world files tractable.',
+    author='Ryo Miyajima',
+    url='https://github.com/sergeant-wizard/artifact_validation',
+)
